6f8bf37e8e953e246eb2698db8fb1d9d2e38feda
lekegitrepo <lekedevrepo@gmail.com>
2020-01-30 06:47:35 -0600

    fix linter error

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const gameBoard = (() => {
-  const board = ['', '', '', '', '', '', '', '', ''];
+  let board = ['', '', '', '', '', '', '', '', ''];
 
   const setBoardTile = (index, value) => {
     if (board[index] === '') {
@@ -8,46 +8,51 @@ const gameBoard = (() => {
   };
 
   const checkRows = () => {
+    let rows = false;
     if (board[0] === board[1] && board[0] === board[2]) {
-      return board[0];
+      rows = board[0];
     } else if (board[3] === board[4] && board[3] === board[5]) {
-      return board[3];
+      rows = board[3];
     } else if (board[6] === board[7] && board[6] === board[8]) {
-      return board[6];
+      rows = board[6];
     }
-    return;
+    return rows;
   };
 
   const checkColumns = () => {
+    let columns = false;
     if (board[0] === board[3] && board[0] === board[6]) {
-      return board[0];
+      columns = board[0];
     } else if (board[1] === board[4] && board[1] === board[7]) {
-      return board[1];
+      columns = board[1];
     } else if (board[2] === board[5] && board[2] === board[8]) {
-      return board[2];
+      columns = board[2];
     }
-    return;
+    return columns;
   };
 
   const checkDiagonals = () => {
+    let diagonal = false;
     if (board[0] === board[4] && board[0] === board[8]) {
-      return board[0];
+      diagonal = board[0];
     } else if (board[2] === board[4] && board[2] === board[6]) {
-      return board[2];
+      diagonal = board[2];
     }
-    return;
+    return diagonal;
   };
 
   const checkWinPattern = () => {
+    let checkWin;
     if (checkColumns()) {
-      return checkColumns();
+      checkWin = checkColumns();
     } else if (checkRows()) {
-      return checkRows();
+      checkWin =  checkRows();
     } else if (checkDiagonals()) {
-      return checkDiagonals();
+      checkWin = checkDiagonals();
     } else if (!board.includes('')) {
-      return false;
+      checkWin =  false;
     }
+    return checkWin;
   };
 
   const resetBoard = () => {
